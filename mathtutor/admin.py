a8c864f9698a766891de123212f9bed21b6123af
Joseph <seidel.jp@gmail.com>
2016-03-31 11:56:22 -0500

    added student QP to admin

--- mathtutor/admin.py
+++ mathtutor/admin.py
@@ -11,6 +11,17 @@ class StudentInline(admin.TabularInline):
 class UserAdmin(OriginalUserAdmin):
     inlines = [StudentInline,]
 
+class StudentAdmin(admin.ModelAdmin):
+    readonly_fields = ('get_username', "quizzes_passed")
+    list_display = ('stuid', 'quizzes_passed')
+    search_fields = ['stuid__username']
+    def get_username(self, obj):
+        return obj.stuid.username
+    def quizzes_passed(self, obj):
+        return obj.get_quiz_progress()['passed']
+
+admin.site.register(Student, StudentAdmin)
+
 try:
     admin.site.unregister(User)
 finally:
